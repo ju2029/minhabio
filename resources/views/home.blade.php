167425ae68b9e54b83400493ca7231aa463a5105
jussara <juh.unimontes@gmail.com>
2022-04-07 23:18:19 -0300

    Reformulando código

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UNES - Página principal</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Olá</h1>
+
+        <a href="https://www.w3schools.com/default.asp">Site Tutoriais</a>
+
+    
+    </body>
+
+
+</html>
+
+
